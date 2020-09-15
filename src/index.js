02693fd6050835ee8a00f33d4d14ef5a77ab27dc
mikohan <angara99@gmail.com>
2020-09-15 13:32:29 +0300

    implemented both server side and client side rendering

--- src/index.js
+++ src/index.js
@@ -1,7 +1,38 @@
-const express = require('express');
+// const express = require('express');
+// const React = require('react');
+// const renderToString = require('react-dom/server').renderToString;
+// const Home = require('./client/components/Home').default;
+// const app = express();
+
+import express from 'express';
+import React from 'react';
+import { renderToString } from 'react-dom/server';
+import Home from './client/components/Home';
+
 const app = express();
 
-app.get('/', (req, res) => {});
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+  const content = renderToString(<Home />);
+
+  const html = `
+  <!DOCTYPE html>
+    <html lang="en">
+        <head>
+            <meta charset="UTF-8">
+            <meta name="viewport" content="width=device-width, initial-scale=1.0">
+            <title>Document</title>
+        </head>
+        <body>
+            <div id='root'>${content}</div>
+            <script src="bundle.js"></script>
+        </body>
+</html>
+  `;
+
+  res.send(html);
+});
 
 app.listen(3000, () => {
   console.log('Listening on port 3000');
